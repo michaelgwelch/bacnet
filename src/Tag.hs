bf597952c9539fdc70bfdfe53c8b1c92cb9f5c4f
Michael Welch <michaelgwelch@gmail.com>
2014-03-22 14:30:40 -0500

    Implement initial set of 'initial octet' inspection functions

--- src/Tag.hs
+++ src/Tag.hs
@@ -1,13 +1,57 @@
 module Tag (
   isAP,
   isCS,
+  lvt,
+  isOpen,
+  openType,
+  isClose,
+  closeType,
+  isExtendedLength,
   ) where
 
 import Data.Word
 import Data.Bits
 
+-- | 'True' if the Class bit (the 3rd bit) is not set
 isAP :: Word8 -> Bool
 isAP = not . isCS
 
+-- | 'True' if the Class bit (the 3rd bit) is set
 isCS :: Word8 -> Bool
 isCS = flip testBit 3
+
+-- | The value B'111', used to mask off the 3 least significant bits
+lvtMask :: Word8
+lvtMask = 0x07
+
+-- | The value B'101' (0x05)
+extendedLength :: Word8
+extendedLength = 0x05
+
+-- | The value B'110' (0x0E)
+openType :: Word8
+openType = 0x06
+
+-- | The value B'111' (0x0F)
+closeType :: Word8
+closeType = 0x07
+
+-- | Returns the length/value/type which is the 3 least significant bits
+lvt :: Word8 -> Word8
+lvt = (.&. lvtMask)
+
+-- | Returns true if the class and length/value/type bits match
+clvtMatches :: Word8 -> Word8 -> Bool
+clvtMatches expected = (== expected) . (.&. 0x0F)
+
+-- | 'True' if 'isCS' and 'lvt' == 'openType'
+isOpen :: Word8 -> Bool
+isOpen = clvtMatches 0x0E
+
+-- | 'True' if 'isCS' and 'lvt' == 'closeType'
+isClose :: Word8 -> Bool
+isClose = clvtMatches 0x0F
+
+-- | 'True' if 'lvt' == 'extendedLength'
+isExtendedLength :: Word8 -> Bool
+isExtendedLength = (== 0x05) . lvt
--- test/TagSpec.hs
+++ test/TagSpec.hs
@@ -1,7 +1,7 @@
 module TagSpec where
 
 import Test.Hspec
-import Test.QuickCheck
+import Test.QuickCheck hiding ((.&.))
 import Data.Bits
 import Tag
 
@@ -22,5 +22,27 @@ spec = do
       (\b -> isAP b `shouldBe` False)
 
   describe "isCS" $
-    it "returns the same value as not . isAP" $
+    it "is equivalent to not . isAP" $
       property (\b -> isCS b `shouldBe` not (isAP b))
+
+  describe "lvt" $ do
+    it "returns 0 with input 0xF0" $ lvt 0xF0 `shouldBe` 0
+    it "returns 1 with input 0x01" $ lvt 0x01 `shouldBe` 1
+    it "returns 2 with input 0xa2" $ lvt 0xa2 `shouldBe` 2
+    it "returns 4 with input 0xbc" $ lvt 0xbc `shouldBe` 4
+    it "returns 7 with input 0xcf" $ lvt 0xcf `shouldBe` 7
+
+    it "is equivalent to (.&. 0x07)" $
+      property (\b -> lvt b `shouldBe` (b .&. 0x07))
+
+  describe "isOpen" $
+    it "returns the same value as isCS b && (lvt b == openType)" $
+      property (\b -> isOpen b `shouldBe` (isCS b && lvt b == openType))
+
+  describe "isClose" $
+    it "returns the same value as isCS b && (lvt b == closeType)" $
+      property (\b -> isClose b `shouldBe` (isCS b && lvt b == closeType))
+
+  describe "isExtendedLength" $
+    it "returns True if lvt == 5, else False" $
+      property (\b -> isExtendedLength b `shouldBe` (lvt b == 5))
