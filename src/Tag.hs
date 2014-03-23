067d6d1473c9cc0815d4f40a40a621fc4e9b7f8f
Michael Welch <michaelgwelch@gmail.com>
2014-03-23 09:50:47 -0500

    Add first test for unsignedAP

--- src/Tag.hs
+++ src/Tag.hs
@@ -1,9 +1,12 @@
 module Tag
   (
   readTag,
+  readAPTag,
   runReader,
   apNullTag,
   apTrueTag,
+  apFalseTag,
+  apUnsignedTag
   ) where
 
 import Control.Exception (assert)
@@ -17,8 +20,13 @@ apNullTag = Tag [0x00]
 apTrueTag :: Tag
 apTrueTag = Tag [0x11]
 
-newtype Reader a = R {runReader :: [Word8] -> Maybe (a, [Word8])}
+apFalseTag :: Tag
+apFalseTag = Tag [0x10]
 
+apUnsignedTag :: Word -> Tag
+apUnsignedTag w | w <= fromIntegral (maxBound :: Word8) = Tag [0x21, fromIntegral w]
+
+newtype Reader a = R {runReader :: [Word8] -> Maybe (a, [Word8])}
 
 
 readTag :: Reader Tag
@@ -27,16 +35,38 @@ readTag = R (\bs -> case bs of
                     (x:xs) -> if isAP x then runReader readAPTag bs
                                         else Nothing)
 
-assertNotEmptyAndAP :: [Word8] -> a -> a
-assertNotEmptyAndAP [] = assert False
-assertNotEmptyAndAP (b:bs) = assert (isAP b)
+assertNotEmptyAndAP :: [Word8] -> Maybe(Tag, [Word8]) -> Maybe(Tag, [Word8])
+assertNotEmptyAndAP [] _ = Nothing
+assertNotEmptyAndAP (b:bs) result = if (isAP b) then result else Nothing
 
--- | reads the next ap encoded tag.
---   Calling conventions: input should not be empty and first byte should not
---   have class bit set.
 readAPTag :: Reader Tag
 readAPTag = R (\bs -> assertNotEmptyAndAP bs $
-                case head bs of
-                  0x00 -> Just (apNullTag, tail bs)
-                  0x11 -> Just (apTrueTag, tail bs)
+                case tagNumber $ head bs of
+                  0 -> readAPNullTag bs
+                  1 -> readAPBoolTag bs
+                  2 -> readAPUnsignedTag bs
+                  3 -> undefined
+                  4 -> undefined
+                  5 -> undefined
+                  6 -> undefined
+                  7 -> undefined
+                  8 -> undefined
+                  9 -> undefined
+                  10-> undefined
+                  11-> undefined
+                  12-> undefined
             )
+
+type ReaderTag' = [Word8] -> Maybe(Tag, [Word8])
+
+readAPNullTag :: ReaderTag'
+readAPNullTag (0x00:bs) = Just(apNullTag, bs)
+readAPNullTag _ = Nothing
+
+readAPBoolTag :: ReaderTag'
+readAPBoolTag (0x10:bs) = Just(apFalseTag, bs)
+readAPBoolTag (0x11:bs) = Just(apTrueTag, bs)
+readAPBoolTag _ = Nothing
+
+readAPUnsignedTag :: ReaderTag'
+readAPUnsignedTag (0x21:b:bs) = Just(Tag [0x21,b], bs)
--- test/TagSpec.hs
+++ test/TagSpec.hs
@@ -6,12 +6,24 @@ import Test.Hspec
 
 spec :: Spec
 spec = do
-  describe "readTag" $ do
-    it "returns Just(apNullTag, bs) on input: 0x00 : bs" $
-      property $ (\bs -> runReader readTag (0x00 : bs) `shouldBe`
-      Just(apNullTag, bs))
+  describe "readAPTag" $ do
     it "returns Nothing for input that is empty" $
-      runReader readTag [] `shouldBe` Nothing
-    it "returns Just(apTrueTag, bs) on input: 0x11 : bs" $
-      property $ (\bs -> runReader readTag (0x11 : bs) `shouldBe`
+      runReader readAPTag [] `shouldBe` Nothing
+    it "returns Just(apNullTag, bs) on input (0x00 : bs)" $
+      property $ (\bs -> runReader readAPTag (0x00 : bs) `shouldBe`
+      Just(apNullTag, bs))
+    it "returns Nothing on 0x0w where w is non-zero" $
+      property $ forAll (choose (1,15))
+      (\b -> runReader readAPTag [b] `shouldBe` Nothing)
+    it "returns Just(apTrueTag, bs) on input (0x11 : bs)" $
+      property $ (\bs -> runReader readAPTag (0x11 : bs) `shouldBe`
       Just(apTrueTag, bs))
+    it "returns Just(apFalseTag, bs) on input (0x10 : bs)" $
+      property $ (\bs -> runReader readAPTag (0x10 : bs) `shouldBe`
+      Just(apFalseTag, bs))
+    it "returns Nothing on 0x1w where w is not 0 or 1" $
+      property $ forAll (choose (2,15))
+      (\b -> runReader readAPTag [(0x10 + b)] `shouldBe` Nothing)
+    it "returns Just(apUnsignedTag 0, bs) on input (0x21 : 0x00 : bs)" $
+      property $ (\bs -> runReader readAPTag (0x21 : 0x00 : bs) `shouldBe`
+      Just(apUnsignedTag 0, bs))
