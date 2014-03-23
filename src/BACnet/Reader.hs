c06d5cebbbd66fea5b8ecfeb0c28a7c436d78b5f
Michael Welch <michaelgwelch@gmail.com>
2014-03-23 15:03:24 -0500

    Renamed readUnsignedAP to readUnsignedAP' and made new readUnsignedAP

--- src/BACnet/Reader.hs
+++ src/BACnet/Reader.hs
@@ -8,8 +8,11 @@ import Data.Word
 import BACnet.Tag
 import BACnet.Reader.Core
 
-readUnsignedAP :: Reader Word
-readUnsignedAP = byte >>= \b -> readUnsignedAPTag b >>= \(UnsignedAP len) ->
-                 bytes (fromIntegral len) >>= \bs ->
-                 let words = map fromIntegral bs in
-                 return $ foldl (\acc w -> acc * 256 + w) 0 words
+readUnsignedAP' :: Reader Word
+readUnsignedAP' = byte >>= \b -> readUnsignedAPTag b >>= \(UnsignedAP len) ->
+                  bytes (fromIntegral len) >>= \bs ->
+                  let words = map fromIntegral bs in
+                  return $ foldl (\acc w -> acc * 256 + w) 0 words
+
+readUnsignedAP :: Reader Word32
+readUnsignedAP = fmap fromIntegral readUnsignedAP'
