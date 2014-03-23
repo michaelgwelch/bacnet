b0c9290524053ed17959bcc0644fa9b909d42978
Michael Welch <michaelgwelch@gmail.com>
2014-03-23 07:45:37 -0500

    Refactor readTag

--- src/Tag.hs
+++ src/Tag.hs
@@ -6,7 +6,9 @@ module Tag
   apTrueTag,
   ) where
 
+import Control.Exception (assert)
 import Data.Word
+import Tag.Core
 data Tag = Tag [Word8] deriving (Show, Eq)
 
 apNullTag :: Tag
@@ -22,6 +24,19 @@ newtype Reader a = R {runReader :: [Word8] -> Maybe (a, [Word8])}
 readTag :: Reader Tag
 readTag = R (\bs -> case bs of
                     [] -> Nothing
-                    (x:xs) -> case x of
-                      0x00 -> Just(Tag [x], xs)
-                      0x11 -> Just(Tag [x], xs))
+                    (x:xs) -> if isAP x then runReader readAPTag bs
+                                        else Nothing)
+
+assertNotEmptyAndAP :: [Word8] -> a -> a
+assertNotEmptyAndAP [] = assert False
+assertNotEmptyAndAP (b:bs) = assert (isAP b)
+
+-- | reads the next ap encoded tag.
+--   Calling conventions: input should not be empty and first byte should not
+--   have class bit set.
+readAPTag :: Reader Tag
+readAPTag = R (\bs -> assertNotEmptyAndAP bs $
+                case head bs of
+                  0x00 -> Just (apNullTag, tail bs)
+                  0x11 -> Just (apTrueTag, tail bs)
+            )
