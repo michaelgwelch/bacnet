e1d06f47ad8c523144ca5708749f8280b08c14e3
Michael Welch <michaelgwelch@gmail.com>
2014-03-31 19:24:43 -0500

    Add some write*Tag functions

--- src/BACnet/Tag.hs
+++ src/BACnet/Tag.hs
@@ -16,15 +16,21 @@ module BACnet.Tag
   Tag(..),
   boolVal,
   tagLength,
-  const'
+  writeNullAPTag,
+  writeBoolAPTag,
+  writeUnsignedAPTag,
   ) where
 
 import Control.Monad
 import Control.Applicative
-import Data.Word
+import Data.Word (Word, Word32, Word16, Word8)
+import Data.Int (Int, Int32, Int16, Int8)
 import qualified BACnet.Tag.Core as TC
 import BACnet.Reader.Core
 import qualified Data.ByteString.Lazy as BS
+import Data.Bits
+import qualified BACnet.Writer.Core as WC
+import BACnet.Writer.Core ((<>))
 
 data Tag =
           NullAP
@@ -187,3 +193,78 @@ tagLength :: Tag -> Word32
 tagLength (UnsignedAP len) = len
 tagLength (SignedAP len) = len
 tagLength (OctetStringAP len) = len
+
+
+
+
+
+-------------------
+
+class Unfoldable a where
+  byteGuard :: a -> Bool
+
+signedByteGuard :: (Ord a, Num a) => a -> Bool
+signedByteGuard n = n >= (-128) && n <= 127
+
+unsignedByteGuard :: (Ord a, Num a) => a -> Bool
+unsignedByteGuard n = n <= 255
+
+instance Unfoldable Word where
+  byteGuard = unsignedByteGuard
+
+instance Unfoldable Word32 where
+  byteGuard = unsignedByteGuard
+
+instance Unfoldable Word16 where
+  byteGuard = unsignedByteGuard
+
+instance Unfoldable Word8 where
+  byteGuard = unsignedByteGuard
+
+instance Unfoldable Int where
+  byteGuard = signedByteGuard
+
+instance Unfoldable Int32 where
+  byteGuard = signedByteGuard
+
+instance Unfoldable Int16 where
+  byteGuard = signedByteGuard
+
+instance Unfoldable Int8 where
+  byteGuard = signedByteGuard
+
+unfoldNum :: (Num a, Unfoldable a, Ord a, Bits a, Integral a)
+  => a -> [Word8] -> (Word32, [Word8])
+unfoldNum n bs
+  | byteGuard n = (1, currentByte : bs)
+  | otherwise =
+    let (len, bs') = unfoldNum (shiftR n 8) (currentByte : bs)
+    in (len+1, bs')
+  where currentByte = fromIntegral (n .&. 0xFF)
+
+-- | Writes a tag appropriate for an application encoded null value
+writeNullAPTag :: WC.Writer
+writeNullAPTag = WC.null
+
+writeBoolAPTag :: Bool -> WC.Writer
+writeBoolAPTag b = WC.unsigned8 (if b then 0x11 else 0x10)
+
+writeUnsignedAPTag :: Word32 -> WC.Writer
+writeUnsignedAPTag 0 = WC.unsigned16 0x2100
+writeUnsignedAPTag v =
+  let initialOctet = 0x20 + len
+      (len, bs)    = unfoldNum v []
+  in WC.bytes ((fromIntegral initialOctet) : bs)
+
+writeSignedAPTag :: Word32 -> WC.Writer
+writeSignedAPTag 0 = WC.signed16 0x3100
+writeSignedAPTag v =
+  let initialOctet = 0x30 + len
+      (len, bs)    = unfoldNum v []
+  in WC.bytes ((fromIntegral initialOctet) : bs)
+
+writeRealAPTag :: Float -> WC.Writer
+writeRealAPTag = (WC.unsigned8 0x44 <>) . WC.real
+
+writeDoubleAPTag :: Double -> WC.Writer
+writeDoubleAPTag = (WC.unsigned16 0x5508 <>) . WC.double
