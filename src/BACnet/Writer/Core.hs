d83e9b57307c950d8b5ba796d66572c01b9c90ea
Michael Welch <michaelgwelch@gmail.com>
2014-03-31 19:25:09 -0500

    Add a bunch of Writer.Core stuff

--- src/BACnet/Writer/Core.hs
+++ src/BACnet/Writer/Core.hs
@@ -3,62 +3,63 @@ module BACnet.Writer.Core
   (
     Writer,
     runW,
-    nullAPTag,
-    boolAPTag,
-    unsignedAPTag,
     empty,
     append,
-    (<>)
+    (<>),
+    null,
+    unsigned8,
+    unsigned16,
+    signed8,
+    signed16,
+    real,
+    double,
+    bytes
   ) where
 
 import qualified Data.ByteString.Lazy as BS
-import Data.ByteString.Lazy.Builder
-import Data.Word
-import Data.Monoid
+import Data.ByteString.Lazy.Builder (
+  word8, word16BE, int8, int16BE, lazyByteString, toLazyByteString,
+  Builder, floatBE, doubleBE)
+import Data.Word (Word8, Word16)
+import Data.Int (Int8, Int16)
+import Data.Monoid (Monoid, (<>), mempty, mappend)
 import Prelude hiding (null)
 import qualified Prelude as P
-import Data.Bits
+import Data.Bits ((.&.))
 import qualified Control.Applicative as A
 import qualified Control.Monad as M
-import BACnet.Writer.ByteString
 
--- | A writer - this type actually provides no value that isn't already
---   provided directly by builder. The tag functions below should go in Tag.hs
---   So this whole file can be deleted.
+-- | A writer
 newtype Writer = W { unWriter :: Builder }
+
+-- | A builder that writes the byte 0x00
+null :: Writer
+null = W $ word8 0x00
+
+unsigned8 :: Word8 -> Writer
+unsigned8 = W . word8
+
+unsigned16 :: Word16 -> Writer
+unsigned16 = W . word16BE
+
+signed8 :: Int8 -> Writer
+signed8 = W . int8
+
+signed16 :: Int16 -> Writer
+signed16 = W . int16BE
+
+bytes :: [Word8] -> Writer
+bytes ws = W $ lazyByteString $ BS.pack ws
+
+real :: Float -> Writer
+real = W . floatBE
+
+double :: Double -> Writer
+double = W . doubleBE
 
 -- | Runs a writer and returns the result as an array of bytes.
 runW :: Writer -> [Word8]
 runW = BS.unpack . toLazyByteString . unWriter
-
-
-null :: Builder
-null = word8 0x00
-
-unfoldWord' :: Word -> BS.ByteString
-unfoldWord' = unfoldl unfoldStep
-  where unfoldStep 0 = Nothing
-        unfoldStep n = Just(fromIntegral (n .&. 0xFF), shiftR n 8)
-
-unfoldWord :: Word -> [Word8] -> (Word32,[Word8])
-unfoldWord 0 bs = (0, bs)
-unfoldWord n bs =
-  let (len,bs') = unfoldWord (shiftR n 8) (fromIntegral (n .&. 0xFF) : bs)
-  in (len+1, bs')
-
--- | Writes a tag appropriate for an application encoded null value
-nullAPTag :: Writer
-nullAPTag = W null
-
-boolAPTag :: Bool -> Writer
-boolAPTag b = W (word8 (if b then 0x11 else 0x10))
-
-unsignedAPTag :: Word32 -> Writer
-unsignedAPTag 0 = W (word16BE 0x2000)
-unsignedAPTag v =
-  let initialOctet = 0x20 + len
-      (len, bs)    = unfoldWord (fromIntegral v) []
-  in W (lazyByteString $ BS.pack $ (fromIntegral initialOctet) : bs)
 
 empty :: Writer
 empty = W (lazyByteString BS.empty)
