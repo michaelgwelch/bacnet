27c145aeadcc1d735c08391adcdf548637598242
Michael Welch <michaelgwelch@gmail.com>
2014-04-06 16:01:15 -0500

    Remove unused line in writeUnsignedAPTag
    Length should never be 0. So the deleted line should never be called.
    One could argue it should raise an error, but what was left in there
    was a remnant from a previous implementation which is just wrong.

--- src/BACnet/Tag.hs
+++ src/BACnet/Tag.hs
@@ -268,7 +268,6 @@ writeBoolAPTag :: Bool -> WC.Writer
 writeBoolAPTag b = WC.unsigned8 (if b then 0x11 else 0x10)
 
 writeUnsignedAPTag :: Word32 -> WC.Writer
-writeUnsignedAPTag 0 = WC.unsigned8 0x21
 writeUnsignedAPTag len | len < 5 = WC.unsigned8 (0x20 + fromIntegral len)
                        | otherwise = WC.unsigned8 0x25 <> WC.unsigned8 (fromIntegral len)
 
