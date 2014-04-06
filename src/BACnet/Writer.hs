cfc1a21c8a304e8be4d9e3400f8877d11c33dbcc
Michael Welch <michaelgwelch@gmail.com>
2014-04-06 13:50:26 -0500

    Add initial version of writeUnsignedAP
    Only the trivial case is tested/implemented.

--- src/BACnet/Writer.hs
+++ src/BACnet/Writer.hs
@@ -2,11 +2,13 @@ module BACnet.Writer
   (
     writeNullAP,
     writeBoolAP,
+    writeUnsignedAP,
     runW
   ) where
 
 import BACnet.Writer.Core
 import BACnet.Tag
+import Data.Word
 import Prelude hiding (null)
 
 writeNullAP :: Writer
@@ -15,3 +17,6 @@ writeNullAP = null
 writeBoolAP :: Bool -> Writer
 writeBoolAP False = unsigned8 0x10
 writeBoolAP True = unsigned8 0x11
+
+writeUnsignedAP :: Word32 -> Writer
+writeUnsignedAP 0 = unsigned8 0x21 <> unsigned8 0x00
--- test/BACnet/WriterSpec.hs
+++ test/BACnet/WriterSpec.hs
@@ -12,8 +12,12 @@ spec =
         runW writeNullAP `shouldBe` [0x00]
 
     describe "writeBoolAP" $ do
-      it "returns [0x10] for Bool input False" $
+      it "writes [0x10] for Bool input False" $
         runW (writeBoolAP False) `shouldBe` [0x10]
 
-      it "returns [0x11] for Bool input True" $
+      it "writes [0x11] for Bool input True" $
         runW (writeBoolAP True) `shouldBe` [0x11]
+
+    describe "writeUnsignedAP" $
+      it "writes [0x21, 0x00] for input 0" $
+        runW (writeUnsignedAP 0) `shouldBe` [0x21, 0x00]
