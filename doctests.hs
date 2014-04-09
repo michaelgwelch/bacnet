76fb7da3ff126a66d034a8ea43b677b21e16151e
Michael Welch <michaelgwelch@gmail.com>
2014-04-08 21:28:44 -0500

    Add doctests.hs
    This will be used to run examples in haddock as tests.

---
+++ doctests.hs
@@ -0,0 +1,5 @@
+import Test.DocTest
+main = doctest ["-ddump-splices", "src/BACnet/Reader/Core.hs",
+  "src/BACnet/Prim.hs", "src/BACnet/Writer/Core.hs",
+  "src/BACnet/Tag/Core.hs",
+  "src/BACnet/Tag.hs", "src/BACnet/Writer.hs"]
